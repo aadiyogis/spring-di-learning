556a70641a9020674cdbc6c74fd2f655699a5d0d
aditya <adityaajmera89@gmail.com>
2022-01-15 12:45:49 +0530

    initial project setup

---
+++ src/main/java/com/example/springdilearning/SpringDiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdilearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdilearning/SpringDiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdilearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
