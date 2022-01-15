aca1bdbb8efd9ec8eee2df1d3d27a12b67d91d52
aditya <adityaajmera89@gmail.com>
2022-01-15 22:50:19 +0530

    using java configuration for initializing primary bean.

--- src/main/java/com/example/springdilearning/configuration/GreetingServiceConfig.java
+++ src/main/java/com/example/springdilearning/configuration/GreetingServiceConfig.java
@@ -1,8 +1,10 @@
 package com.example.springdilearning.configuration;
 
 import com.example.springdilearning.service.ConstructorGreetingServiceImpl;
+import com.example.springdilearning.service.PrimaryGreetingServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -10,5 +12,11 @@ public class GreetingServiceConfig {
     @Bean
     public ConstructorGreetingServiceImpl constructorGreetingServiceImpl() {
         return new ConstructorGreetingServiceImpl();
+    }
+
+    @Primary
+    @Bean
+    public PrimaryGreetingServiceImpl primaryGreetingServiceImpl() {
+        return new PrimaryGreetingServiceImpl();
     }
 }
--- src/main/java/com/example/springdilearning/service/PrimaryGreetingServiceImpl.java
+++ src/main/java/com/example/springdilearning/service/PrimaryGreetingServiceImpl.java
@@ -1,10 +1,5 @@
 package com.example.springdilearning.service;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
 public class PrimaryGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
