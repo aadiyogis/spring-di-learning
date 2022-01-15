6fdf2197a5a96b3cb73c16d41dd7f278affdf9c4
aditya <adityaajmera89@gmail.com>
2022-01-15 15:10:36 +0530

    primary bean using primary annotation, default goto bean.

--- src/main/java/com/example/springdilearning/controller/MyController.java
+++ src/main/java/com/example/springdilearning/controller/MyController.java
@@ -1,12 +1,19 @@
 package com.example.springdilearning.controller;
 
+import com.example.springdilearning.service.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String helloWorld() {
         System.out.println("Hello World");
-        return "Hi";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/com/example/springdilearning/service/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.springdilearning.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hi Primary ----!!!!!!!!!!";
+    }
+}
